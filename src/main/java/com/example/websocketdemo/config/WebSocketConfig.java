894127ae03c1f01400c3cacf4dbb58e3622e5574
mghaneh1375 <mghaneh1375@gmail.com>
2022-03-13 17:46:11 +0330

    first

--- src/main/java/com/example/websocketdemo/config/WebSocketConfig.java
+++ src/main/java/com/example/websocketdemo/config/WebSocketConfig.java
@@ -14,7 +14,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry
                 .addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost")
+//                .setAllowedOrigins("http://localhost")
+                .setAllowedOrigins("http://185.239.106.26")
                 .withSockJS();
     }
 
