75fbf02f5c5e21aef9f2146b420171c6a6572f73
mohammad ghane <mghaneh1375@yahoo.com>
2022-05-07 10:40:35 +0430

    cors policy added.

--- src/main/java/com/example/websocketdemo/security/CorsFilter.java
+++ src/main/java/com/example/websocketdemo/security/CorsFilter.java
@@ -28,25 +28,25 @@ public class CorsFilter implements Filter {
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Allow-Headers", "Authorization, Cache-Control, Content-Type, Origin, X-Requested-With, Accept");
         response.setHeader("Access-Control-Max-Age", "3600");
-//
+
 //        //SEND OK or validate
         if ("OPTIONS".equalsIgnoreCase(((HttpServletRequest) req).getMethod())) {
 
-//            if(((HttpServletRequest) req).getHeader("Origin") == null ||
-//                !((HttpServletRequest) req).getHeader("Origin").equals("https://okft.org"))
-//            {
-//                response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-//                return;
-//            }
+            if(((HttpServletRequest) req).getHeader("Origin") == null ||
+                !((HttpServletRequest) req).getHeader("Origin").equals("https://okft.org"))
+            {
+                response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                return;
+            }
 
             response.setStatus(HttpServletResponse.SC_OK);
         } else {
-//            if(((HttpServletRequest) req).getHeader("Referer") == null ||
-//                !((HttpServletRequest) req).getHeader("Referer").contains("https://okft.org"))
-//            {
-//                response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-//                return;
-//            }
+            if(((HttpServletRequest) req).getHeader("Referer") == null ||
+                !((HttpServletRequest) req).getHeader("Referer").contains("https://okft.org"))
+            {
+                response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                return;
+            }
             chain.doFilter(req, res);
         }
     }
