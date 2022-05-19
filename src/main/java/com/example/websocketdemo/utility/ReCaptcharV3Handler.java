124e72bc6812f0b0694fdc27daeec0ee93e0cf9d
mghaneh1375 <mghaneh1375@gmail.com>
2022-05-19 16:29:13 +0430

    file uploading added.

---
+++ src/main/java/com/example/websocketdemo/utility/ReCaptcharV3Handler.java
@@ -0,0 +1,51 @@
+package bogen.austria.Utility;
+
+import bogen.austria.Exception.InvalidFieldsException;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+public class ReCaptcharV3Handler {
+
+    private String secretKey = "6Le4dHQfAAAAAEFT0BP4xGdlNH5l_OOyoZwi-c9Y";
+    private String serverAddress = "https://www.google.com/recaptcha/api/siteverify";
+
+//    private String secretKey = "6LdAHAAgAAAAACrlCuqSBFuP4CI_l3SBRbtOUlsB";
+//    private String serverAddress = "https://www.google.com/recaptcha/api/siteverify";
+
+    public float verify(String recaptchaFormResponse) throws InvalidFieldsException {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.add("secret",secretKey);
+        map.add("response",recaptchaFormResponse);
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map,headers);
+
+        RestTemplate restTemplate = new RestTemplate();
+        ReCaptchaResponse response = restTemplate.postForObject(
+                serverAddress,request, ReCaptchaResponse.class);
+
+
+//        System.out.println("Success: " + response.isSuccess());
+//        System.out.println("Score: " + response.getScore());
+
+//        if (response.getErrorCodes() != null){
+//            System.out.println("Error codes: ");
+//            for (String errorCode: response.getErrorCodes()){
+//                System.out.println("\t" + errorCode);
+//            }
+//        }
+
+        if (response == null || !response.isSuccess()){
+            throw new InvalidFieldsException("Invalid ReCaptha. Please check site");
+        }
+        // return 0.4f;
+        return response.getScore();
+    }
+}
